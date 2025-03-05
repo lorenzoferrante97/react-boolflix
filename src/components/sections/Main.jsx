1adcf2c28a0a16760aafe672fd06bd0b7738f580
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-05 13:04:14 +0100

    add: main structure

--- src/components/sections/Main.jsx
+++ src/components/sections/Main.jsx
@@ -1,8 +1,12 @@
 export default function Main() {
   return (
     <>
-      <main>
-        <p>Sono il main</p>
+      <main className='container-fluid bg-smoke-950/100 h-screen'>
+        <section className='row-grid'>
+          <div className='bg-smoke-700 col-span-full h-[80px] md:col-span-4'></div>
+          <div className='bg-smoke-700 col-span-full h-[80px] md:col-span-4'></div>
+          <div className='bg-smoke-700 col-span-full h-[80px] md:col-span-4'></div>
+        </section>
       </main>
     </>
   );

2cd3f6d9b97fb9c6939e93590def17927ef13619
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-05 11:57:21 +0100

    add: build api + fetch api movies

--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -5,6 +5,14 @@ const GlobalContext = createContext();
 
 // create provider
 const GlobalProvider = ({ children }) => {
+  // import .env variables
+  const tmdbApiMoviesUrl = import.meta.env.VITE_TMDB_API_MOVIES_URL;
+  const tmdbApiSeriesUrl = import.meta.env.VITE_TMDB_API_SERIES_URL;
+  const tmdbApiToken = import.meta.env.VITE_TMDB_API_TOKEN;
+  // build apis
+  const tmdbApiSearchMovies = `${tmdbApiMoviesUrl}&api_key=${tmdbApiToken}`;
+  const tmdbApiSearchSeries = `${tmdbApiSeriesUrl}&api_key=${tmdbApiToken}`;
+
   // useState
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredMovies, setFilteredMovies] = useState([]);
@@ -14,6 +22,16 @@ const GlobalProvider = ({ children }) => {
   const handleField = e => {
     const { value } = e.target;
     setSearchQuery(value);
+  };
+
+  // fetch api filtered movies
+  const filterMovies = () => {
+    fetch(`${tmdbApiSearchMovies}&query=${searchQuery}`)
+      .then(response => response.json())
+      .then(data => {})
+      .catch(error => {
+        console.error(error);
+      });
   };
 
   const value = {};

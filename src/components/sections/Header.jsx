5c9d776c036952b7998ed9a1b55e1c51b7bff644
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-05 12:46:18 +0100

    add: header content + style

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -1,4 +1,8 @@
+import { GlobalProvider, useGlobalContext } from "../../contexts/GlobalContext";
+
 export default function Header() {
+  const { handleField } = useGlobalContext();
+
   return (
     <>
       <header className='container-fluid mt-10u max-md:gap-y-5u items-center md:flex-row md:justify-between'>
@@ -7,13 +11,18 @@ export default function Header() {
           BoolFlix
         </span>
         {/* search */}
-        <form className='w-full md:max-w-[40%]'>
+        <form className='gap-3u flex w-full flex-col md:max-w-[60%] md:flex-row'>
           <input
             type='text'
             placeholder='Cerca un film o serie'
-            className='px-5u py-u min-h-12u font-body-s-regular w-full rounded-full border border-white/40 focus:border-white focus:outline-white'
-            // onChange={}
+            className='px-5u py-u min-h-12u font-body-s-regular w-full rounded-md border border-white/40 focus:border-white focus:outline-white'
+            onChange={handleField}
           />
+          <button
+            type='submit'
+            className='min-h-12u font-body-base-bold px-7u py-4u w-full rounded-full bg-red-400 text-white md:w-fit'>
+            Cerca
+          </button>
         </form>
       </header>
     </>

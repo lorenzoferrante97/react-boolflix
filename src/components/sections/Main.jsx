60f360b62587c239699ea228f9bee347f0925e11
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-05 14:51:03 +0100

    add: movies section

--- src/components/sections/Main.jsx
+++ src/components/sections/Main.jsx
@@ -1,12 +1,18 @@
 export default function Main() {
   return (
     <>
-      <main className='container-fluid bg-smoke-950/100 h-screen'>
+      <main className='container-fluid bg-smoke-950/100 gap-20u py-10u h-screen'>
+        {/* filtered movies */}
         <section className='row-grid'>
-          <div className='bg-smoke-700 col-span-full h-[80px] md:col-span-4'></div>
-          <div className='bg-smoke-700 col-span-full h-[80px] md:col-span-4'></div>
+          <div className='col-span-full'>
+            <h2 className='font-h2 uppercase'>Film</h2>
+            <p className='font-body-s-light text-smoke-400 uppercase'>
+              Risultati di ricerca:
+            </p>
+          </div>
           <div className='bg-smoke-700 col-span-full h-[80px] md:col-span-4'></div>
         </section>
+        {/* filtered series */}
       </main>
     </>
   );
